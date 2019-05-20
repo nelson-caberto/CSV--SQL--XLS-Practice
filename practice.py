1a4e4636e4761f26eb28729caff7caf3fcbdd53e
Nelson Caberto <nelson-caberto@users.noreply.github.com>
2019-05-19 19:48:58 -0500

    Added Excel Output

--- practice.py
+++ practice.py
@@ -2,6 +2,10 @@ import sqlite3
 import sys
 import csv
 
+#pip install openpyxl
+from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
+
 def createDBConnection(db_file):
 	"""
 	returns handle for accessing db as a file or in memory
@@ -91,22 +95,22 @@ if __name__ == '__main__':
 		
 ### COMPUTE ###
 
-	char6s = ['A', 'B', 'C', 'D', 'E', 'F']
+	char6s = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 	char2s = ['A', 'B']
 	float8s = [
 		#[row, min, max],
-		(7, 0, 19239),
-		(8, 19240, 24439),
-		(9, 24440, 30679),
-		(10, 30680, 38999),
-		(11, 39000, 49919),
-		(12, 49920, 62919),
-		(13, 62920, 80079),
-		(14, 80080, 101919),
-		(15, 101920, 128959),
-		(16, 128960, 163799),
-		(17, 163800, 207999),
-		(18, 208000, sys.maxsize)
+		(7, 0, 19999),
+		(8, 20000, 24999),
+		(9, 25000, 30999),
+		(10, 31000, 39999),
+		(11, 40000, 49999),
+		(12, 50000, 69999),
+		(13, 70000, 99999),
+		(14, 100000, 109999),
+		(15, 110000, 129999),
+		(16, 130000, 169999),
+		(17, 170000, 209999),
+		(18, 210000, sys.maxsize)
 	]
 
 	# Get a list possibilities of int4a in this dataset
@@ -119,7 +123,6 @@ if __name__ == '__main__':
 		print(f'Exception in _query: {e} on {sqlSTMT}')
 	int4as = [int4a[0] for int4a in dbCursor.fetchall()]
 	int4as.sort()
-
 
 	results = {}
 	for int4a in int4as:
@@ -141,18 +144,147 @@ if __name__ == '__main__':
 					results[int4a][char2][char6][row] = dbCursor.fetchone()[0]
 
 	totals = {}
-	for (row, min, max) in float8s:
-		sqlSTMT = f'SELECT COUNT(*) FROM a WHERE float8 BETWEEN {min} and {max}'
-		try:
-			dbCursor.execute(sqlSTMT)
-		except sqlite3.Error as e:
-			print(f'Database error: {e} on {sqlSTMT}')
-			continue
-		except Exception as e:
-			print(f'Exception in _query: {e} on {sqlSTMT}')
-			continue
-		totals[row] = dbCursor.fetchone()[0]
+	for int4a in int4as:
+		totals[int4a] = {}
+		for (row, min, max) in float8s:
+			sqlSTMT = f'SELECT COUNT(*) FROM a WHERE int4a={int4a} AND float8 BETWEEN {min} and {max};'
+			try:
+				dbCursor.execute(sqlSTMT)
+			except sqlite3.Error as e:
+				print(f'Database error: {e} on {sqlSTMT}')
+				continue
+			except Exception as e:
+				print(f'Exception in _query: {e} on {sqlSTMT}')
+				continue
+			totals[int4a][row] = dbCursor.fetchone()[0]
 
 	dbCursor.close()
 	
 ### OUTPUT ###
+
+wb = Workbook()
+ws = wb.active
+offset = 0
+
+cell = lambda column, row: get_column_letter(column+offset)+str(row)
+
+for int4 in int4as:
+
+	#A
+	ws[cell(1,1)] = 'int4'
+	ws[cell(1,7)] = f'{int4}'
+
+	#B
+	ws[cell(2,1)] = 'float8'
+	ws[cell(2,7)] = f'{float8s[0][2]} and under'
+	ws[cell(2,8)] = f'{float8s[1][1]} - {float8s[1][2]}'
+	ws[cell(2,9)] = f'{float8s[2][1]} - {float8s[2][2]}'
+	ws[cell(2,10)] = f'{float8s[3][1]} - {float8s[3][2]}'
+	ws[cell(2,11)] = f'{float8s[4][1]} - {float8s[4][2]}'
+	ws[cell(2,12)] = f'{float8s[5][1]} - {float8s[5][2]}'
+	ws[cell(2,13)] = f'{float8s[6][1]} - {float8s[6][2]}'
+	ws[cell(2,14)] = f'{float8s[7][1]} - {float8s[7][2]}'
+	ws[cell(2,15)] = f'{float8s[8][1]} - {float8s[8][2]}'
+	ws[cell(2,16)] = f'{float8s[9][1]} - {float8s[9][2]}'
+	ws[cell(2,17)] = f'{float8s[10][1]} - {float8s[10][2]}'
+	ws[cell(2,18)] = f'{float8s[11][1]} and over'
+
+	#C
+	ws[cell(3,1)] = 'Number of id'
+	ws[cell(3,2)] = 'char6 A'
+	ws[cell(3,3)] = 'int4 A'
+	ws[cell(3,5)] = 'char2'
+	ws[cell(3,6)] = 'A'
+	result = results[int4]['A']['C']
+	for row in result: ws[cell(3,row)] = result[row]
+
+	#D
+	ws[cell(4,5)] = 'char2 B'
+	ws[cell(4,6)] = 'B'
+	result = results[int4]['B']['C']
+	for row in result: ws[cell(4,row)] = result[row]
+
+	#E
+	ws[cell(5,3)] = ''
+	ws[cell(5,4)] = 'char2 A'
+	ws[cell(5,5)] = ''
+	ws[cell(5,6)] = 'C'
+	result = results[int4]['A']['G']
+	for row in result: ws[cell(5,row)] = result[row]
+
+	#F
+	ws[cell(6,5)] = ''
+	ws[cell(6,6)] = 'D'
+	result = results[int4]['A']['B']
+	for row in result: ws[cell(6,row)] = result[row]
+
+	#G
+	ws[cell(7,5)] = ''
+	ws[cell(7,6)] = 'E'
+	result = results[int4]['A']['E']
+	for row in result: ws[cell(7,row)] = result[row]
+
+	#H
+	ws[cell(8,5)] = ''
+	ws[cell(8,6)] = 'F'
+	result = results[int4]['A']['A']
+	for row in result: ws[cell(8,row)] = result[row]
+
+	#I
+	ws[cell(9,5)] = ''
+	ws[cell(9,6)] = 'G'
+	result = results[int4]['A']['D']
+	for row in result: ws[cell(9,row)] = result[row]
+
+	#J
+	ws[cell(10,5)] = ''
+	ws[cell(10,6)] = 'H'
+	result = results[int4]['A']['F']
+	for row in result: ws[cell(10,row)] = result[row]
+
+	#K
+	ws[cell(11,4)] = ''
+	ws[cell(11,5)] = ''
+	ws[cell(11,6)] = 'I'
+	result = results[int4]['B']['G']
+	for row in result: ws[cell(11,row)] = result[row]
+
+	#L
+	ws[cell(12,5)] = ''
+	ws[cell(12,6)] = 'J'
+	result = results[int4]['B']['B']
+	for row in result: ws[cell(12,row)] = result[row]
+
+	#M
+	ws[cell(13,5)] = ''
+	ws[cell(13,6)] = 'K'
+	result = results[int4]['B']['E']
+	for row in result: ws[cell(13,row)] = result[row]
+
+	#N
+	ws[cell(14,5)] = ''
+	ws[cell(14,6)] = 'L'
+	result = results[int4]['B']['A']
+	for row in result: ws[cell(14,row)] = result[row]
+
+	#O
+	ws[cell(15,5)] = ''
+	ws[cell(15,6)] = 'M'
+	result = results[int4]['B']['D']
+	for row in result: ws[cell(15,row)] = result[row]
+
+	#P
+	ws[cell(16,5)] = ''
+	ws[cell(16,6)] = 'N'
+	result = results[int4]['B']['F']
+	for row in result: ws[cell(16,row)] = result[row]
+
+	#Q
+	ws[cell(17,2)] = 'Total Col A-N'
+	ws[cell(17,6)] = 'O'
+	total = totals[int4]
+	for row in total: ws[cell(17,row)] = total[row]
+	
+	offset += 17
+
+wb.save("output0.xlsx")
